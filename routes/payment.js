ca8030d95fc0abfcf26fb7696bfd56a6d224eca3
Shemdoe <georgehmariki@gmail.com>
2026-05-07 00:59:02 +0300

    done

--- routes/payment.js
+++ routes/payment.js
@@ -52,7 +52,7 @@ router.post('/api/pay', async (req, res) => {
     //disable Vodacom for now due to frequent issues
     if (networkBrand === 'vodacom') {
       res.set('HX-Reswap', 'none');
-      return res.render('fragments/payment-form-error', { layout: false, message: 'Changamoto ya mtandao imetokea Vodacom. Tafadhali tumia Tigo, Airtel, au Halotel.' });
+      return res.render('fragments/payment-form-error', { layout: false, message: 'Kuna changamoto ya mtandao Vodacom. Tafadhali tumia Tigo, Airtel, au Halotel.' });
     }
 
     try {
